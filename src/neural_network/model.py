49daf38d40c49c7a8e6bc1e262627ac60f8c880f
Adrian Lachowicz <adrianlachowicz36@gmail.com>
2023-04-22 16:18:05 +0200

    Reformated code and deleted unused lines

--- src/neural_network/model.py
+++ src/neural_network/model.py
@@ -26,50 +26,25 @@ def build_model(name: str, hidden_layers: int, use_dropout: bool, dropout_value:
     output_dim = 7
 
     for i in range(hidden_layers):
-        hidden_dim = 2 ** (5+(hidden_layers - i-1))
+        hidden_dim = 2 ** (5 + (hidden_layers - i - 1))
 
         if i == 0:
             # Create input layer
-            model.add(
-                Dense(hidden_dim, activation="relu", input_shape=(input_dim,))
-            )
+            model.add(Dense(hidden_dim, activation="relu", input_shape=(input_dim,)))
 
             # If user wants dropout, add it
             if use_dropout:
-                model.add(
-                    Dropout(dropout_value)
-                )
+                model.add(Dropout(dropout_value))
 
         elif i == (hidden_layers - 1):
             # Create output layer
-            model.add(
-                Dense(output_dim, activation="softmax")
-            )
+            model.add(Dense(output_dim, activation="softmax"))
         else:
             # Create hidden layers
-            model.add(
-                Dense(hidden_dim, activation="relu")
-            )
+            model.add(Dense(hidden_dim, activation="relu"))
 
             # If user wants dropout, add it
             if use_dropout:
-                model.add(
-                    Dropout(dropout_value)
-                )
+                model.add(Dropout(dropout_value))
 
     return model
-
-
-
-X_train, X_test, y_train, y_test = load_and_split_dataset(
-    "../../data/covtype.data", 0.7
-)
-
-model = build_model("test", 4, False, 0.5)
-model.compile(optimizer="Adam",
-              loss="sparse_categorical_crossentropy",
-              metrics=["accuracy"])
-
-
-print(model.summary())
-model.fit(X_train, y_train)
